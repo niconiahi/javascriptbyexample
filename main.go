4c8b04eb0a57c7547aa56c42461c80b9a05e1789
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-01-16 15:16:01 -0300

    feat: render the documentation next to the code accordingly

--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"path"
 )
 
+//go:embed public
+var files embed.FS
+
 func main() {
-	port := "8000"
-	publicDir := "public"
-	fmt.Printf("Serving Go by Example at http://127.0.0.1:%s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir(publicDir))))
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fsys, err := fs.Sub(files, "public")
+		if err != nil {
+			log.Fatal(err)
+		}
+		p := r.URL.Path
+		fmt.Printf("%v\n", p)
+		if path.Ext(p) == "" {
+			fmt.Printf("is html %v\n", p)
+			p = p + ".html"
+			w.Header().Set("Content-Type", "text/html")
+		}
+		if path.Ext(p) == ".css" {
+			fmt.Printf("is css %v\n", p)
+			w.Header().Set("Content-Type", "text/css")
+		}
+		content, err := fs.ReadFile(fsys, p[1:])
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		w.Write(content)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
--- src/generate.go
+++ src/generate.go
@@ -5,133 +5,226 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
+	"io/fs"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+	"text/template"
 
 	"github.com/alecthomas/chroma/v2"
 	"github.com/alecthomas/chroma/v2/formatters/html"
 	"github.com/alecthomas/chroma/v2/lexers"
-
-	// "github.com/alecthomas/chroma/v2/lexers"
 	"github.com/alecthomas/chroma/v2/styles"
 	"github.com/russross/blackfriday/v2"
 )
 
 const publicDir = "./public"
 const examplesDir = "./examples"
+const templatesDir = "./templates"
 
 type Lexer string
 
 const (
-	Javascript Lexer = "javascript"
-	Console    Lexer = "console"
+	LexerJavascript Lexer = "javascript"
 )
 
 var docsRegex = regexp.MustCompile(`^(\s*(\/\/|#)\s|\s*\/\/$)`)
 
 type Segment struct {
 	Documentation string
-	CodeLines     []string
+	Code          string
+}
+
+type Example struct {
+	ID       string
+	Name     string
+	Hash     string
+	Segments []Segment
+	Next     *string
+	Previous *string
+}
+
+func makeExamples() []Example {
+	examples := []Example{}
+	entries := readDir(examplesDir)
+	for i, entry := range entries {
+		example := Example{}
+		info, err := entry.Info()
+		if err != nil {
+			panic("not possible to get file info for example")
+		}
+		example.ID = info.Name()
+		example.Name = info.Name()
+		example.Next = getNextId(i, entries)
+		example.Previous = getPreviousId(i, entries)
+		jsFilePath := composePath(entry.Name(), "js")
+		if isFile(jsFilePath) {
+			segments := makeSegments(jsFilePath)
+			example.Segments = segments
+		}
+		content := readFile(jsFilePath)
+		hash := makeHash(content)
+		example.Hash = hash
+		examples = append(examples, example)
+	}
+	return examples
+}
+
+func makeTerminal(example Example) []byte {
+	jsFilePath := composePath(example.ID, "js")
+	command := exec.Command(
+		"deno", "run",
+		"--allow-read",
+		jsFilePath,
+	)
+	output, err := command.CombinedOutput()
+	if err != nil {
+		panic("could not execute the deno command for the js example file")
+	}
+	prompt := []byte("$ deno run " + example.ID + ".js" + "\n")
+	prompt = append(prompt, output...)
+	return prompt
+}
+
+func makeHashFile(example Example) {
+	shFilePath := composePath(example.ID, "sh")
+	hashFilePath := composePath(example.ID, "hash")
+	if isFile(hashFilePath) {
+		prevHash := readFile(hashFilePath)
+		if example.Hash != string(prevHash) {
+			fmt.Printf("recreating hash file\n")
+			terminal := makeTerminal(example)
+			writeFile(
+				shFilePath,
+				[]byte(terminal),
+			)
+			writeFile(
+				hashFilePath,
+				[]byte(example.Hash),
+			)
+		} else {
+			fmt.Printf("hash files match. Omitting creating files\n")
+		}
+	} else {
+		fmt.Printf("creating fresh hash file\n")
+		terminal := makeTerminal(example)
+		writeFile(
+			shFilePath,
+			[]byte(terminal),
+		)
+		writeFile(
+			hashFilePath,
+			[]byte(example.Hash),
+		)
+	}
+}
+
+func makeCss() []byte {
+	style := getStyle()
+	formatter := html.New(html.WithClasses(true))
+	var buffer bytes.Buffer
+	err := formatter.WriteCSS(&buffer, style)
+	if err != nil {
+		panic("failed to generate CSS")
+	}
+	return buffer.Bytes()
+}
+
+func getStyle() *chroma.Style {
+	return styles.Get("tokyonight-night")
+}
+
+func makeCodeBlock(code string) string {
+	lexer := lexers.Get(string(LexerJavascript))
+	formatter := html.New(html.WithClasses(true))
+	iterator, err := lexer.Tokenise(nil, code)
+	if err != nil {
+		panic("there was an error while tokenasing the code")
+	}
+	buffer := bytes.NewBuffer([]byte{})
+	style := getStyle()
+	err = formatter.Format(buffer, style, iterator)
+	if err != nil {
+		panic("there was an error while formatting the code")
+	}
+	return buffer.String()
+}
+
+type Row struct {
+	Markdown  string
+	CodeBlock string
+}
+type PageDataExample struct {
+	Name     string
+	Next     *string
+	Previous *string
+	Rows     []Row
+}
+type PageTemplate string
+
+const (
+	TemplateExample = "example"
+	TemplateIndex   = "index"
+)
+
+func makeTemplate(t PageTemplate) *template.Template {
+	s := string(t)
+	tmpl := template.New(s + ".tmpl")
+	return template.Must(tmpl.ParseFiles(templatesDir + "/" + s + ".tmpl"))
+}
+
+func makeFiles(examples []Example) {
+	for _, example := range examples {
+		var rows []Row
+		description := Row{
+			Markdown:  example.ID,
+			CodeBlock: example.ID,
+		}
+		rows = append(rows, description)
+		for _, segment := range example.Segments {
+			markdown := makeMarkdown(segment.Documentation)
+			codeBlock := makeCodeBlock(segment.Code)
+			row := Row{
+				Markdown:  markdown,
+				CodeBlock: codeBlock,
+			}
+			rows = append(rows, row)
+		}
+		t := makeTemplate(TemplateExample)
+		var buffer bytes.Buffer
+		err := t.Execute(&buffer, PageDataExample{
+			Name:     example.Name,
+			Next:     example.Next,
+			Previous: example.Previous,
+			Rows:     rows,
+		})
+		if err != nil {
+			fmt.Printf("%v\n", err.Error())
+			panic("error while generating the page template")
+		}
+		writeFile(publicDir+"/"+example.ID+".html", buffer.Bytes())
+		makeHashFile(example)
+	}
 }
 
 func main() {
 	if !isDir(examplesDir) {
 		panic("examples path is not a dir")
 	}
-	examples := readDir(examplesDir)
-	for _, example := range examples {
-		info, err := example.Info()
-		if err != nil {
-			panic("not possible to get file info for example")
-		}
-
-		jsFilePath := examplesDir + "/" + example.Name() + "/" + info.Name() + ".js"
-		shFilePath := examplesDir + "/" + example.Name() + "/" + info.Name() + ".sh"
-		hashFilePath := examplesDir + "/" + example.Name() + "/" + info.Name() + ".hash"
-
-		command := exec.Command(
-			"deno", "run",
-			"--allow-read",
-			jsFilePath,
-		)
-		output, err := command.CombinedOutput()
-		if err != nil {
-			panic("could not execute the deno command for the js example file")
-		}
-
-		prompt := []byte("$ deno run " + info.Name() + ".js" + "\n")
-		content := append(prompt, output...)
-		hash := makeHash(content)
-		if isFile(jsFilePath) {
-			segments := makeSegments(jsFilePath)
-			for _, segment := range segments {
-				markdown := makeMarkdown(segment.Documentation)
-				fmt.Printf("%v \n", markdown)
-				codeBlock := strings.Join(segment.CodeLines, "\n")
-				// fmt.Printf("codeblock %v\n", codeBlock)
-				// lexer := makeShellLexer()
-				// lexer = chroma.Coalesce(lexer)
-				lexer := lexers.Get(string(Javascript))
-				style := styles.Get("monokai")
-				if style == nil {
-					panic("there is no style for the given name")
-				}
-				formatter := html.New(html.WithClasses(true))
-				iterator, err := lexer.Tokenise(nil, codeBlock)
-				// fmt.Printf("iterator %v\n", iterator)
-				if err != nil {
-					panic("there was an error while tokenasing the code")
-				}
-				buffer := bytes.NewBuffer([]byte{})
-				err = formatter.Format(buffer, style, iterator)
-				if err != nil {
-					panic("there was an error while formatting the code")
-				}
-				fmt.Printf("formatted %v\n", buffer.String())
-			}
-		}
-		if isFile(hashFilePath) {
-			prevHash := readFile(hashFilePath)
-			if hash != prevHash {
-				fmt.Printf("recreating hash file\n")
-				writeFile(
-					shFilePath,
-					[]byte(content),
-				)
-				writeFile(
-					hashFilePath,
-					[]byte(hash),
-				)
-			} else {
-				fmt.Printf("hash files match. Omitting creating files\n")
-			}
-		} else {
-			fmt.Printf("creating fresh hash file\n")
-			writeFile(
-				shFilePath,
-				[]byte(content),
-			)
-			writeFile(
-				hashFilePath,
-				[]byte(hash),
-			)
-		}
-	}
+	examples := makeExamples()
+	css := makeCss()
+	writeFile(publicDir+"/code.css", css)
+	makeFiles(examples)
 }
 
-// func getLexer(path string) string {
-// 	if strings.HasSuffix(path, ".js") {
-// 		return "javascript"
-// 	} else {
-// 		return "console"
-// 	}
-// }
+func composePath(id string, extension string) string {
+	return examplesDir + "/" + id + "/" + id + "." + extension
+}
 
 func readLines(path string) []string {
 	content := readFile(path)
-	return strings.Split(content, "\n")
+	return strings.Split(string(content), "\n")
 }
 
 func makeSegments(path string) []Segment {
@@ -143,14 +236,18 @@ func makeSegments(path string) []Segment {
 		matchesCode := !matchesDocs
 		matchesEmptyLine := strings.TrimSpace(line) == ""
 		if matchesEmptyLine {
-			segments = append(segments, nextSegment)
+			segments = append(
+				segments,
+				nextSegment,
+			)
 			nextSegment = Segment{}
 		}
 		if matchesDocs {
 			nextSegment.Documentation = strings.Trim(line, " /")
 		}
 		if matchesCode {
-			nextSegment.CodeLines = append(nextSegment.CodeLines, line)
+			nextCode := fmt.Sprintf("%v\n%v", nextSegment.Code, line)
+			nextSegment.Code = strings.TrimSpace(nextCode)
 		}
 	}
 	return segments
@@ -187,12 +284,12 @@ func readDir(path string) []os.DirEntry {
 	return entries
 }
 
-func readFile(path string) string {
+func readFile(path string) []byte {
 	content, err := os.ReadFile(path)
 	if err != nil {
 		panic("not possible to find the previous hash file")
 	}
-	return string(content)
+	return content
 }
 
 func isFile(path string) bool {
@@ -232,4 +329,26 @@ func makeShellLexer() chroma.Lexer {
 		},
 	)
 	return lexer
+}
+
+func getPreviousId(index int, entries []fs.DirEntry) *string {
+	if index == 0 {
+		return nil
+	}
+	id := entries[index-1].Name()
+	return &id
+}
+
+func getNextId(index int, entries []fs.DirEntry) *string {
+	if index >= len(entries)-1 {
+		return nil
+	}
+	id := entries[index+1].Name()
+	return &id
+}
+
+func makeName(id string) string {
+	firstLetter := id[0:1]
+	rest := id[1:]
+	return fmt.Sprintf("%v%v", strings.ToUpper(firstLetter), strings.ToLower(rest))
 }
